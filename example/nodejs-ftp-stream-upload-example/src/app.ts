c4262cc5f8b184e994999a9b8b93ba02afcf9731
박흥준 Rock <rockheung@gmail.com>
2021-04-22 21:45:55 +0900

    Success but only pdf file

--- example/nodejs-ftp-stream-upload-example/src/app.ts
+++ example/nodejs-ftp-stream-upload-example/src/app.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import fs from "fs";
 import path from "path";
+import { Client } from "basic-ftp";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 
@@ -19,8 +21,36 @@ app.get("/file", (req: Request, res: Response) => {
 });
 
 app.post("/file", (req: Request, res: Response) => {
-  console.log(JSON.stringify(req.headers, null, 2));
-  res.status(201).send("Ok");
+  const client = new Client();
+  client.ftp.verbose = true;
+  // Log progress for any transfer from now on.
+  client.trackProgress((info) => {
+    console.log("File", info.name);
+    console.log("Type", info.type);
+    console.log("Transferred", info.bytes);
+    console.log("Transferred Overall", info.bytesOverall);
+  });
+  if (req.readable) {
+    client
+      .access({
+        host: "bluejun73.cafe24.com",
+        port: 21,
+        user: "bluejun73",
+        password: "Kyafglc36(",
+      })
+      .then(() => client.uploadFrom(req, uuidv4() + ".png"))
+      .then(() => {
+        console.log("Done!");
+        res.status(201).send("Done!");
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Damn");
+      })
+      .finally(() => {
+        client.close();
+      });
+  }
 });
 
 app.listen(8080);
